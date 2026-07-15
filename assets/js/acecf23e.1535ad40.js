fb81bb0b304c0dd33e857de3d815af0fce188481
Serg-Kv <Sergei.Kosyrev@onlyoffice.com>
2026-07-15 16:19:16 +0500

    preview: update build

---
+++ assets/js/acecf23e.1535ad40.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapi_onlyoffice_com=self.webpackChunkapi_onlyoffice_com||[]).push([[81903],{101912:o=>{o.exports=JSON.parse('{"blogBasePath":"/api.onlyoffice.com/changelog","blogTitle":"Changelog","authorsListPath":"/api.onlyoffice.com/changelog/authors"}')}}]);
